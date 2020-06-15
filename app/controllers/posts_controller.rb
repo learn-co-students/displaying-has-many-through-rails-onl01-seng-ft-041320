67eeee0e6585ed764b6c196141039c62c73e33cc
webdevjscoder <cobianjuan19@gmail.com>
2020-06-15 14:42:27 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
 
   def show
+  end
+
+  private
+
+  def find_post
     @post = Post.find(params[:id])
   end
 end
